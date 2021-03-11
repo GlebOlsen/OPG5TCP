20a6c381754d70cc61e99593fb81b14b5491b5e3
Gleb Olsen <gleb.olsen@gmail.com>
2021-03-11 17:34:42 +0100

    Add project files.

---
+++ OPG5TCP/Beer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPG5TCP
+{
+    public class Beer
+    {
+        // 4 ins
+        private string _name;
+        private int _id;
+        private double _price;
+        private double _adv;
+
+        // 4 props
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) throw new ArgumentException();
+                _name = value;
+            }
+        }
+
+        public int ID
+        {
+            get => _id;
+            set { _id = value; }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException();
+                _price = value;
+            }
+        }
+
+        public double Adv
+        {
+            get => _adv;
+            set
+            {
+                if (value >= 100 || value <= 0) throw new ArgumentOutOfRangeException();
+                _adv = value;
+            }
+        }
+
+        // Constructor for 4 props
+        public Beer(string name, int id, double price, double adv)
+        {
+            Name = name;
+            ID = id;
+            Price = price;
+            Adv = adv;
+        }
+        // Const for server
+        public Beer() { }
+        // og en ToString til at get by id:
+        public override string ToString()
+        {
+            return "Name: " + Name + " Id: " + ID + " Price: " + Price + " %: " + Adv;
+        }
+    }
+}
---
+++ OPG5TCP/Server.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OPG5TCP
+{
+    public class Server
+    {
+        private static List<Beer> _beers = new List<Beer>()
+        {
+            new Beer{Name = "Heineken",Adv = 5,ID = 1,Price = 10},
+            new Beer{Name = "Carlsberg",ID = 2, Price = 11.95,Adv = 4.6}
+        };
+
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 4646);
+            server.Start();
+            Console.WriteLine("Server is running...");
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client is connected to the server...");
+                Task.Run(() =>
+                {
+                    TcpClient tempSocket = client;
+                    DoClient(tempSocket);
+                });
+            }
+        }
+
+        private void DoClient(TcpClient tempSocket)
+        {
+            Stream ns = tempSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+            string message = sr.ReadLine();
+            while (message != " " && message != "stop")
+            {
+                switch (message)
+                {
+                    case "GetAll":
+                        sw.WriteLine(JsonConvert.SerializeObject(_beers));
+                        break;
+                    case "Get": 
+                        int id = Convert.ToInt32(sr.ReadLine());
+                        sw.WriteLine(_beers.Find(beer => beer.ID == id));
+                        break;
+                    case "Save":
+                        string json = sr.ReadLine();
+                        Beer savedBeer = JsonConvert.DeserializeObject<Beer>(json);
+                        _beers.Add(savedBeer);
+                        break;
+                }
+                message = sr.ReadLine();
+            }
+            ns.Close();
+            tempSocket.Close();
+        }
+    }
+}
